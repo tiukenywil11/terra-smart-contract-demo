418ef7f396db219ae123fa5d0c87da67f16ce31f
Kenywil Tiu <tiukenywil11@gmail.com>
2022-04-08 17:13:42 +0800

    Added 'my-terra-dapp' terra smart contract using quickstart guide

---
+++ my-terra-dapp/keys.terrain.js
@@ -0,0 +1,9 @@
+// can use `process.env.SECRET_MNEMONIC` or `process.env.SECRET_PRIV_KEY`
+// to populate secret in CI environment instead of hardcoding
+
+module.exports = {
+  bombay: {
+    mnemonic:
+	"clog practice dad swamp impact own object demise gate online use table now piano general dumb arena spell fabric truly check boost denial worry"  
+  },
+};
