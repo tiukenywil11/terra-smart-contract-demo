8b7e0310b82e6bbd820ee77755bacae6f0fa5784
Kenywil Tiu <tiukenywil11@gmail.com>
2022-04-15 15:02:44 +0800

    Added a call to terra luna testnet, with sample message send

--- in-depth/my-first-contract-react/index.js
+++ in-depth/my-first-contract-react/index.js
@@ -1,5 +1,6 @@
 // LCDClient: helps connect to the LCD, Coin: Helps work with assets
-const {LCDClient, Coin } = require('@terra-money/terra.js');
+const {LCDClient, Coin, MnemonicKey, MsgSend } = require('@terra-money/terra.js');
+
 
 // create a new connection to the LCD called terra 
 const terra = new LCDClient({
@@ -7,6 +8,19 @@ const terra = new LCDClient({
     chainID: 'localterra',
 });
 
+const terraTestNet = new LCDClient({
+    URL: 'https://bombay-lcd.terra.dev',
+    chainID: 'bombay-12',
+});
+
+// adding dummy wallet mnemonic key to test POST requests
+const mk = new MnemonicKey({
+    mnemonic: 'clog practice dad swamp impact own object demise gate online use table now piano general dumb arena spell fabric truly check boost denial worry'
+});
+
+// create a terra wallet instance, using mnemonic key stated above
+const wallet = terraTestNet.wallet(mk);
+    
 // create a new coin, with swap rate linked to USD
 // swap rate parameter 1,000,000 is equal to 1
 const offerCoin = new Coin('uusd', '1000000');
@@ -19,3 +33,33 @@ const swapRateFunction = async() => {
 };
 
 swapRateFunction();
+
+const sendTransactionToLCD = async() => {
+
+    const address = 'terra13885pj0s4sgv0dcpk6r2ftph750h8jlt88m0ej';
+    const [balance] = await terraTestNet.bank.balance(address);
+    console.log(balance.toData());
+
+    // create a transaction
+    // MsgSend, takes 3 parameters, 'wallet from, wallet to, and transfer amount
+    // in this case, send to self 0.1 usd
+    const my_tx = new MsgSend(
+        'terra13885pj0s4sgv0dcpk6r2ftph750h8jlt88m0ej',
+        'terra13885pj0s4sgv0dcpk6r2ftph750h8jlt88m0ej',
+        { uluna: 10000 },
+    );
+
+    // create and sign the transaction
+    // createAndSignTx takes 2 parameters, msgs: which is usally the transcation, and memo: memo message with the transaction
+    const messageSending = await wallet.createAndSignTx({
+        msgs:[my_tx],
+        memo: 'test transaction for terra'
+    });
+
+    // broadcast the message to the LCD [sending to testnet]
+    const result = await terraTestNet.tx.broadcast(messageSending);
+
+    console.log(result.txhash);
+}
+
+sendTransactionToLCD();
